9f1b6ef9a6de81ab16328e4c1b67a21050672c7b
Kharmalina Tong <kharmalinat7@gmail.com>
2022-10-24 12:41:19 -0400

    fetch pokemon API data via data.results array

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
 
@@ -6,6 +6,29 @@ const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
+  // initialize state
+  const [pokemonList, setpokemonList] = useState([]);
+
+  // add useEffect here ⬇️
+  useEffect(() => {
+  // declare the async data fetching function
+  const fetchData = async () => {
+  // get the data from the api
+  const res = await fetch(pokeApi);
+  // convert the data to json
+  const data = await res.json();
+
+  // set state with the result
+  setpokemonList(data.results);
+  console.log(pokemonList)
+  // console.log(typeof cards);
+}
+
+  // call the function
+  fetchData()
+    // make sure to catch any error
+    .catch(console.error);;
+  }, []);
 
   return (
     <div data-testid="app">
